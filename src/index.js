d06e1036e777bf1ba172c766fe1d1226fa24deda
RafaDraw <www.rafadraw@gmail.com>
2018-12-21 16:47:43 -0200

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Arquivos SASS
+import './scss/index.scss';
+
+// Dependecias
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquigos JS
+// Tatamento de Includes
+import './js/core/includes'
+// City Buttons
+import './js/plugins/cityButtons'
